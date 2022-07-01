b367b07b5ea01613276ec50031420901c17cfaa8
Arkesh009 <arkeshbhargava@gmail.com>
2022-07-01 09:00:00 +0530

    hypotenous

---
+++ Rivision/01.cpp
@@ -0,0 +1,17 @@
+// Calculate the hypotenous of the triangle
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a, b, c;
+    cout << "Enter the sides of the triangle\n";
+    cin >> a >> b;
+
+    // square root of a^2 + b^2
+    c = sqrt(pow(a,2) + pow(b,2));
+
+    cout << "Hypotenous of the triangle = " << c;
+
+    return 0;
+}
