d41973c9505740c1a3038e9b8a511bdcb6da761f
Arkesh009 <arkeshbhargava@gmail.com>
2025-02-11 23:15:32 +0530

    creadit card validator program

--- sortArr.cpp
+++ sortArr.cpp
@@ -1,24 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void sortArr(int arr[], int size);
+
 int main()
 {
-    int arr[] = {10,2,1,5,6,8,4,9,7,3};
-    int size = sizeof(arr)/sizeof(int);
-    for(int j=0; j<size; j++){
-        for(int i=0; i<size; i++){
-        if(arr[i] > arr[i+1]){
-            int temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-        }else{
-            i++;
-        }
-    }
+    int arr[] = {10,12,19,11,13,16,18,15,14,17};
+    int size = sizeof(arr)/sizeof(arr[0]);
     
+    sortArr(arr, size);
+
+    for(int i: arr){
+        cout << i << "\n";
     }
 
-    for(int i=0; i<size; i++){
-        cout << arr[i] <<"\n";
-    }
 return 0;
+}
+
+void sortArr(int arr[], int size){
+
+    int temp;
+    for(int j=0; j<size-1; j++){
+        for(int i=0; i<size-j-1; i++){
+            if(arr[i] > arr[i+1]){
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+
 }
