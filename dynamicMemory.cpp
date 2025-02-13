acaec8cb0e79b0643f160d80a86b59c759e8fb6f
Arkesh009 <arkeshbhargava@gmail.com>
2025-02-13 23:15:56 +0530

    dynamic memory allocation and delocation

---
+++ dynamicMemory.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int *p = NULL;
+    p = new int[10];
+
+    cout << "Enter 10 numbers:\n";
+    for(int i=0; i<10; i++){
+        cin >> p[i];
+    }
+
+    for(int i=0; i<10; i++){
+        cout << "Iteration Number: " << p[i] << '\n';
+    }
+
+    delete[] p;
+
+return 0;
+}
