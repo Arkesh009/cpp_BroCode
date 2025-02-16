88f2a2754a895f88842014b7b247ca51156b658b
Arkesh009 <arkeshbhargava@gmail.com>
2025-02-17 00:33:17 +0530

    Getters and Setters

---
+++ getterAndsetter.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class stove{
+    private:
+    int temprature = 0;
+
+    public:
+    stove(int temprature){
+        setTemp(temprature);
+    }
+    int getTemp(){
+        return temprature;
+    }
+
+    void setTemp(int temprature){
+        this -> temprature = 100;
+    }
+};
+
+int main()
+{
+   stove stove1(0);
+
+   stove1.setTemp(200);
+
+   cout << "The temprature is: " << stove1.getTemp();
+   
+   return 0;
+}
