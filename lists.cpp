7bca76a67098d9c9446d320fc4c10cac07ef05ca
Arkesh009 <arkeshbhargava@gmail.com>
2025-02-20 00:33:21 +0530

    Lists

--- lists.cpp
+++ lists.cpp
@@ -1,24 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   // List declaration 
-   list<int> ls;
-   ls.push_back(30);
-   ls.emplace_back(40);
+    // List declaration
+    list<int> ls;
+    ls.push_back(30);
+    ls.emplace_back(40);
 
-   for(auto i: ls){
-    cout << i << "\n";
-   }
-   
-   cout << "Updating list:\n";
-   ls.push_front(20);
-   ls.emplace_front(10);
+    for (auto i : ls)
+    {
+        cout << i << "\n";
+    }
 
-   for(auto i: ls){
-    cout << i << "\n";
-   }
+    cout << "Updating list:\n";
+    ls.push_front(20);
+    ls.emplace_front(10);
 
-   return 0;
+    for (auto i : ls)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
 }
