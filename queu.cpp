efc65c19b9fa4180a14b0597cc9a0014498575cd
Arkesh009 <arkeshbhargava@gmail.com>
2025-02-21 00:06:29 +0530

    any message

--- queu.cpp
+++ queu.cpp
@@ -1,19 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   queue<int> q;
-   q.push(1);
-   q.push(2);
-   q.emplace(4);
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.emplace(4);
 
-   q.back() += 5;
+    q.back() += 5;
 
-   cout << q.back() << "\n";
-   cout << q.front() << "\n";
+    cout << q.back() << "\n";
+    cout << q.front() << "\n";
 
-   q.pop();
-   cout << q.front();
-   return 0;
+    q.pop();
+    cout << q.front();
+    return 0;
 }
