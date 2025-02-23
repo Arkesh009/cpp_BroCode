afc42bba5ae80a0d0bc16a33773f638893e4d5ee
Arkesh009 <arkeshbhargava@gmail.com>
2025-02-24 00:01:34 +0530

    Maps

--- map.cpp
+++ map.cpp
@@ -17,15 +17,27 @@ int main()
    mp2.insert({2, {3,4}});
    mp3.emplace(make_pair(2,3), 3);
 
-   // printing map 1
+   // printing map1
    for(auto i: mp1){
     cout << i.first << "," << i.second << "\n";
    }
    cout << "\n";
-   // printing map 2
+   // printing map2
    for(auto i: mp2){
     cout << i.second.first << "\n";
    }
+   cout << "\n";
+   // prints the value saved at key 1 i.e 2
+   cout << mp1[1] << "\n";
 
+   //prints null or 0
+   cout << mp1[5] << "\n";
+
+   auto it1 = mp1.find(3);
+   cout << (*it1).first;
+
+   auto it = mp2.find(2);
+   cout << it->second.first;
+   // erase, swap, size, empty are same
    return 0;
 }
---
+++ multiMap.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // sorted 
+    // but not unique
+    // can store multiple values for single key
+   multimap<int, int> mtmp;
+   mtmp.insert({1,1});
+   mtmp.insert({1,2});
+   mtmp.insert({1,3});
+
+   cout << (*mtmp.find(1)).second;
+   return 0;
+}
