436d7701185241e3343afd4865c3ca87208673ed
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-01 22:59:56 +0530

    overloadedfunc

--- BasicCpp/overloadedFunc.cpp
+++ BasicCpp/overloadedFunc.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void pizza();
@@ -11,15 +11,18 @@ int main()
     pizza("Marghrita");
     pizza("veg", "non-veg");
 
-return 0;
+    return 0;
 }
 
-void pizza(){
+void pizza()
+{
     cout << "Here is your pizza\n";
 }
-void pizza(string topping){
+void pizza(string topping)
+{
     cout << "This is your " << topping << " pizza\n";
 }
-void pizza(string type1, string type2){
+void pizza(string type1, string type2)
+{
     cout << "You have choosen " << type1 << " and " << type2 << " pizza\n";
 }
