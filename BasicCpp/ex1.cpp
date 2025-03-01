634659f107fd837cc72a4f6a6a21d44039e04771
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-01 22:55:35 +0530

    any message

--- BasicCpp/ex1.cpp
+++ BasicCpp/ex1.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -6,9 +6,9 @@ int main()
 
     cin >> a >> b;
 
-    c = sqrt(pow(a,2) + pow(b,2));
+    c = sqrt(pow(a, 2) + pow(b, 2));
 
     cout << c;
 
-return 0;
+    return 0;
 }
