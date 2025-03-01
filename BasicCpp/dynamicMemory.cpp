cf62b9033e53d2f16710a2fd766dc0371ced5ce4
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-01 22:53:40 +0530

    any message

--- BasicCpp/dynamicMemory.cpp
+++ BasicCpp/dynamicMemory.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -6,15 +6,17 @@ int main()
     p = new int[10];
 
     cout << "Enter 10 numbers:\n";
-    for(int i=0; i<10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         cin >> p[i];
     }
 
-    for(int i=0; i<10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         cout << "Iteration Number: " << p[i] << '\n';
     }
 
     delete[] p;
 
-return 0;
+    return 0;
 }
