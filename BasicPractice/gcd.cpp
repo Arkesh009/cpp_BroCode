66d0995f11b5196b83ed0711b32f4d36a774603e
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-02 22:29:01 +0530

    GCD

--- BasicPractice/gcd.cpp
+++ BasicPractice/gcd.cpp
@@ -1,22 +1,24 @@
 // Write a program to find the GCD of two numbers using a function.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b){
-    if(b == 0){
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
         return a;
     }
-    return gcd(b, a%b);
+    return gcd(b, a % b);
 }
 
 int main()
 {
-   int a, b;
-   cout << "Enter two num\n";
-   cin >> a >> b;
+    int a, b;
+    cout << "Enter two num\n";
+    cin >> a >> b;
 
-   cout << "The GCD of " << a << " and " << b << " = " << gcd(a,b);
-   
-   return 0;
+    cout << "The GCD of " << a << " and " << b << " = " << gcd(a, b);
+
+    return 0;
 }
