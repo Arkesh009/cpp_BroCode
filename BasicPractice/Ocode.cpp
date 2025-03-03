152b49702243fbb2f69e86923215e5f9c0aa211b
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-03 23:18:17 +0530

    IO code

--- BasicPractice/Ocode.cpp
+++ BasicPractice/Ocode.cpp
@@ -1,20 +1,24 @@
 // Write a program to take user input for two numbers and print the larger one.
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void findmax(int,int);
+void findmax(int, int);
 
 int main()
 {
-   int a,b;
-   cout << "Enter two num:" ;
-   cin >> a >> b;
-   findmax(a,b);
-   return 0;
+    int a, b;
+    cout << "Enter two num:";
+    cin >> a >> b;
+    findmax(a, b);
+    return 0;
 }
 
-void findmax(int a, int b){
-    if(a>b) cout << "The greater num is: " << a << "\n";
-    else if(b>a) cout << "The greater num is: " << b << "\n";
-    else cout << "Enter valid and different values" << "\n";
+void findmax(int a, int b)
+{
+    if (a > b)
+        cout << "The greater num is: " << a << "\n";
+    else if (b > a)
+        cout << "The greater num is: " << b << "\n";
+    else
+        cout << "Enter valid and different values" << "\n";
 }
