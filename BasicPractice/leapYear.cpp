925342be297c6c998b7083c62a96935502d3bb6c
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-03 23:17:44 +0530

    leapYear

--- BasicPractice/leapYear.cpp
+++ BasicPractice/leapYear.cpp
@@ -1,27 +1,32 @@
 // Write a program to check if a given year is a leap year.
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 // Function declaration
 string leapYear(int year);
 
 int main()
 {
-   int year;
-   cout << "Enter the year: ";
-   cin >> year;
-   cout << year << leapYear(year) << "\n";
-   return 0;
+    int year;
+    cout << "Enter the year: ";
+    cin >> year;
+    cout << year << leapYear(year) << "\n";
+    return 0;
 }
 
-string leapYear(int year){
+string leapYear(int year)
+{
     // check if year is divisible by 4
-    if(year%4 == 0){
+    if (year % 4 == 0)
+    {
         // if it is divisible by 100
-        if(year%100 == 0){
+        if (year % 100 == 0)
+        {
             // then it should also be divisible by 400
-            if(year%400 == 0) return " is a leap year";
+            if (year % 400 == 0)
+                return " is a leap year";
             // if not divisible by 400 but 100 then its not a leap year
-            else return " is not a leap year";
+            else
+                return " is not a leap year";
         }
         return " is a leap year";
     }
