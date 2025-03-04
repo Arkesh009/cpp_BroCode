9e338be2fd3598139d0eff7e4d9e6c4eabe1ec38
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-04 23:24:42 +0530

    isPalindrome

--- BasicPractice/isPalindrome.cpp
+++ BasicPractice/isPalindrome.cpp
@@ -1,13 +1,17 @@
 // Write a program to check if a string is a palindrome.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isPalindrome(string &s){
+// boolean func to to check palindrome
+bool isPalindrome(string &s)
+{
     int start = 0;
-    int end = s.length()-1;
-    while(start < end){
-        if(s[start] != s[end]){
+    int end = s.length() - 1;
+    while (start < end)
+    {
+        if (s[start] != s[end])
+        {
             return false;
         }
         start++;
@@ -17,16 +21,18 @@ bool isPalindrome(string &s){
 }
 int main()
 {
-   string s;
+    string s;
     cout << "Enter the string\n";
-    getline(cin,s);
+    getline(cin, s);
 
-    if(isPalindrome(s)){
+    if (isPalindrome(s))
+    {
         cout << "its a palindrome\n";
     }
-    else{
+    else
+    {
         cout << "its not\n";
     }
 
-   return 0;
+    return 0;
 }
