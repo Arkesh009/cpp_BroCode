4392a56c288405d383d36623dc6e8c5407ce2fed
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-06 00:02:04 +0530

    recheck

--- BasicPractice/spcficRemove.cpp
+++ BasicPractice/spcficRemove.cpp
@@ -1,35 +1,38 @@
 // Implement a function that removes all occurrences of a specific value from a vector.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void removeOccurance(vector<int> &, int target);
 int main()
 {
-   vector<int> v;
-   cout << "Enter the vector size\n";
-   int n;
-   cin >> n;
+    vector<int> v;
+    cout << "Enter the vector size\n";
+    int n;
+    cin >> n;
 
-   cout << "Enter vector elements\n";
-   for(int i=0; i<n; i++){
+    cout << "Enter vector elements\n";
+    for (int i = 0; i < n; i++)
+    {
         int input;
         cin >> input;
         v.push_back(input);
-   }
+    }
 
-   cout << "\nEnter the target: ";
-   int target;
-   cin >> target;
-  
-   removeOccurance(v, target);
-   cout << "Vector after removing target\n";
-   for(auto i: v){
-    cout << i << " ";
-   }
-   return 0;
+    cout << "\nEnter the target: ";
+    int target;
+    cin >> target;
+
+    removeOccurance(v, target);
+    cout << "Vector after removing target\n";
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    return 0;
 }
 
-void removeOccurance(vector<int> &v, int target){
+void removeOccurance(vector<int> &v, int target)
+{
     v.erase(find(v.begin(), v.end(), target), v.end());
 }
