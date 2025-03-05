143368916220aa3c279ccb1200d7eb834f3d5070
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-05 23:29:01 +0530

    any message

--- BasicPractice/sortUsingVec.cpp
+++ BasicPractice/sortUsingVec.cpp
@@ -1,32 +1,35 @@
 // Write a program to input n integers into a vector and then sort the vector in both ascending and descending order.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n;
-   vector<int> v;
-   cout << "Enter the size of vector\n";
-   cin >> n;
+    vector<int> v;
+    cout << "Enter the size of vector\n";
+    cin >> n;
 
-   cout << "Enter the elements\n";
-   for(int i=0; i<n; i++){
+    cout << "Enter the elements\n";
+    for (int i = 0; i < n; i++)
+    {
         int a;
         cin >> a;
         v.push_back(a);
-   }
+    }
 
-   cout << "In ascending order\n";
-   sort(v.begin(), v.end());
-   for(auto i: v){
-    cout << i << "\n";
-   }
+    cout << "In ascending order\n";
+    sort(v.begin(), v.end());
+    for (auto i : v)
+    {
+        cout << i << "\n";
+    }
 
-   cout << "\nIn decending order\n";
-   sort(v.begin(), v.end(), greater<int>());
-   for(auto i: v){
-    cout << i << "\n";
-   }
-   return 0;
+    cout << "\nIn decending order\n";
+    sort(v.begin(), v.end(), greater<int>());
+    for (auto i : v)
+    {
+        cout << i << "\n";
+    }
+    return 0;
 }
