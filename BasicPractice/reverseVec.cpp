6859e02641871de2a982dbf530fd2aeb1d0ca598
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-06 23:49:22 +0530

    reverse vec

---
+++ BasicPractice/reverseVec.cpp
@@ -0,0 +1,17 @@
+// Reverse a vector using reverse() from STL and print the reversed vector.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   vector<int> v = {1,2,3,4,5,6,7,8};
+
+   reverse(v.begin(), v.end());
+
+   for(auto i: v){
+        cout << i << " ";
+   }
+
+   return 0;
+}
