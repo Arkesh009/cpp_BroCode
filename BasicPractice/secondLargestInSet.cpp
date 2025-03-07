fa0b7b2354bda9b76d76c20c1a028c2d3924142b
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-07 23:17:31 +0530

    any message

--- BasicPractice/secondLargestInSet.cpp
+++ BasicPractice/secondLargestInSet.cpp
@@ -1,30 +1,32 @@
 // Implement a function to find the second-largest element in a set.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void secMax(set<int> &s);
 
 int main()
 {
-   set<int> s;
-   cout << "Enter the 8 elements of set\n";
-   
-   for(int i=0; i<8; i++){
+    set<int> s;
+    cout << "Enter the 8 elements of set\n";
+
+    for (int i = 0; i < 8; i++)
+    {
         int inp;
         cin >> inp;
         s.insert(inp);
-   }
+    }
 
-   secMax(s);
-   
-   return 0;
+    secMax(s);
+
+    return 0;
 }
 
-void secMax(set<int> &s){
+void secMax(set<int> &s)
+{
     int size = s.size();
     auto it = s.rbegin();
     it++;
 
-    cout << "The second largest digit in the set = " << *it << "\n" ;
+    cout << "The second largest digit in the set = " << *it << "\n";
 }
