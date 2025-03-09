ff3dd7f44bd9afca9f56c5ca2e2b073de43a24f2
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-09 21:57:31 +0530

    Count freq

--- BasicPractice/cntFreqMap.cpp
+++ BasicPractice/cntFreqMap.cpp
@@ -1,29 +1,39 @@
 // Write a program to count the frequency of each character in a string using a map.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void countFreq(string &, int);
 
 int main()
 {
-   string s;
-   cout << "Enter the string\n";
-   getline(cin, s);
+    // Input the string
+    string s;
+    cout << "Enter the string\n";
+    getline(cin, s);
 
-   countFreq(s,s.length());
+    // calling function
+    countFreq(s, s.length());
 
-   return 0;
+    return 0;
 }
 
-void countFreq(string &s, int n){
+// function defination
+void countFreq(string &s, int n)
+{
+    // creating map
     map<char, int> m;
-    for(auto i: s){
+    for (auto i : s)
+    {
+        // reading each character
         char c = i;
+        // incrementing the character occurance
         m[c]++;
     }
 
-    for(auto i : m){
+    // Printing the output
+    for (auto i : m)
+    {
         cout << i.first << " : " << i.second << "\n";
     }
 }
