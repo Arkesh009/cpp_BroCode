cd10ea02b9f035b4c019cd314194bf4f648bad06
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-10 23:05:12 +0530

    unordered_map Phonebook

--- BasicPractice/phBookUnorderdMap.cpp
+++ BasicPractice/phBookUnorderdMap.cpp
@@ -1,27 +1,35 @@
 // Implement a simple phonebook using unordered_map where you can store and look up phone numbers by name.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void phBookInput(unordered_map<string, long long int>&, int n);
-void showPhBook(unordered_map<string, long long int>&, int n);
+void phBookInput(unordered_map<string, long long int> &, int n);
+
+void showPhBook(unordered_map<string, long long int> &, int n);
+
 int main()
 {
-   unordered_map<string, long long int> ph;
-   cout << "Enter the size of the phonebook\n";
-   int n;
-   cin >> n;
-   cin.ignore();
-   cout << "Enter the name and number of the contact\n";
-   phBookInput(ph,n);
-   cout << "\n---PHONE BOOK---\n";
-   showPhBook(ph,n);
+    unordered_map<string, long long int> ph;
+    cout << "Enter the size of the phonebook\n";
+    int n;
+    cin >> n;
+    // clear input buffer
+    cin.ignore();
+    cout << "Enter the name and number of the contact\n";
 
-   return 0;
+    // Function calls
+    phBookInput(ph, n);
+    cout << "\n---PHONE BOOK---\n";
+    showPhBook(ph, n);
+
+    return 0;
 }
 
-void phBookInput(unordered_map<string, long long int> &ph, int n){
-    while(n--){
+// Input function
+void phBookInput(unordered_map<string, long long int> &ph, int n)
+{
+    while (n--)
+    {
         long long int num;
         string name;
         cout << "Enter name: ";
@@ -29,13 +37,16 @@ void phBookInput(unordered_map<string, long long int> &ph, int n){
         cout << "Enter number: ";
         cin >> num;
         cin.ignore();
-        
+
         ph.emplace(name, num);
     }
 }
 
-void showPhBook(unordered_map<string, long long int>& ph, int n){
-    for(auto i: ph){
+// Output function
+void showPhBook(unordered_map<string, long long int> &ph, int n)
+{
+    for (auto i : ph)
+    {
         cout << i.first << ": " << i.second << "\n";
     }
 }
