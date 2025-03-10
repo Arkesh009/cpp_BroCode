fc833ba84da4b01c5ec077bdb28aa748ef64c376
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-10 23:20:58 +0530

    gcd

--- BasicPractice/gcd.cpp
+++ BasicPractice/gcd.cpp
@@ -3,8 +3,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b){
-    if (b == 0){
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
         return a;
     }
     return gcd(b, a % b);
