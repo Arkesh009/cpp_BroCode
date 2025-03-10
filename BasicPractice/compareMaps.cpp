d57b07991c8e17d92a86541ee6443ea0b699f60d
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-10 22:38:23 +0530

    areEqual map

--- BasicPractice/compareMaps.cpp
+++ BasicPractice/compareMaps.cpp
@@ -1,25 +1,32 @@
 // Implement a program that checks if two maps are equal (i.e., contain the same key-value pairs).
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool areEqual(map<string,int>&, map<string, int>&);
+bool areEqual(map<string, int> &, map<string, int> &);
 
 int main()
 {
-   map<string, int> m1 = {{"a",1}, {"b", 2},  {"c", 3}};
-   map<string, int> m2 = {{"a",1}, {"b", 2},  {"c", 3}};
+    // declaration of maps
+    map<string, int> m1 = {{"a", 1}, {"b", 2}, {"c", 3}};
+    map<string, int> m2 = {{"a", 1}, {"b", 2}, {"c", 3}};
 
-   if(areEqual(m1,m2)){
+    // if function returns true
+    if (areEqual(m1, m2))
+    {
         cout << "The maps are equal\n";
-   }
-   else{
+    }
+    // if it returns false
+    else
+    {
         cout << "Not equal\n";
-   }
+    }
 
-   return 0;
+    return 0;
 }
 
-bool areEqual(map<string,int>& m1, map<string, int>& m2){
-    return m1==m2;
+// compare functions
+bool areEqual(map<string, int> &m1, map<string, int> &m2)
+{
+    return m1 == m2;
 }
