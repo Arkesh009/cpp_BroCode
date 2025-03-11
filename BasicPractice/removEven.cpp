2ce9b870a918696271438bcb2b1c907caa87f4c4
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-11 23:21:09 +0530

    remove even from list

--- BasicPractice/removEven.cpp
+++ BasicPractice/removEven.cpp
@@ -1,41 +1,48 @@
 // Write a program to input n integers into a list, and then remove all even numbers from the list.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void removeEven(list<int> &);
 
 int main()
 {
-   list<int> l;
-   cout << "Enter the size of list\n";
-   int size;
-   cin >> size;
+    list<int> l;
+    cout << "Enter the size of list\n";
+    int size;
+    cin >> size;
 
-   cout << "Enter the elements in the list\n";
-
-   while(size--){
-        int n=0;
+    cout << "Enter the elements in the list\n";
+    // input list
+    while (size--)
+    {
+        int n = 0;
         cin >> n;
         l.push_back(n);
-   }
+    }
+    // call function
+    removeEven(l);
 
-   removeEven(l);
-
-   cout << "The list of only odd numbers = ";
-   for(auto i:l){
+    cout << "The list of only odd numbers = ";
+    for (auto i : l)
+    {
         cout << i << " ";
-   }
+    }
 
-   return 0;
+    return 0;
 }
 
-void removeEven(list<int> &l){
-    for(auto i=l.begin(); i!=l.end();){
-        if(*i%2 == 0){
-            i = l.erase(i); //erase returns the next itrator
+// function defination
+void removeEven(list<int> &l)
+{
+    for (auto i = l.begin(); i != l.end();)
+    {
+        if (*i % 2 == 0)
+        {
+            i = l.erase(i); // erase returns the next itrator
         }
-        else{
+        else
+        {
             i++;
         }
     }
