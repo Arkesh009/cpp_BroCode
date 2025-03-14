e0eff4954abeec4cb2ae58fc0aab4bea96004bbc
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-14 22:39:34 +0530

    m

--- BasicPractice/slidingWndwDeque.cpp
+++ BasicPractice/slidingWndwDeque.cpp
@@ -1,10 +1,11 @@
 // Use deque to solve the sliding window maximum problem for an array.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+// slinding window max
 int main()
 {
-   
-   return 0;
+
+    return 0;
 }
