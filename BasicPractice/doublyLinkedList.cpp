449d00a1b204d1715d4af94222fd703491c5b119
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-15 23:28:41 +0530

    doubly linked list

--- BasicPractice/doublyLinkedList.cpp
+++ BasicPractice/doublyLinkedList.cpp
@@ -1,6 +1,6 @@
 // Create a doubly linked list of integers using list and perform insertions and deletions at both ends.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void pushFront(list<int> &, int n);
@@ -10,74 +10,79 @@ void popBack(list<int> &);
 
 int main()
 {
-   list<int> dl;
-   
-   cout << "-----------------\n";
-   cout << "1. Push Front\n2. Push Back\n3. Pop Front\n4. Pop back\n0. Exit\n";
-    
-   for(int a = true; a != 0;){
-    cout << "Select an option\n";
-    int option;
-    cin >> option;
-    if(option == 1){
-         cout << "Enter the element: ";
-         int i;
-         cin >> i;
-         pushFront(dl, i);
-    }
-    else if(option == 2){
-     cout << "Enter the element: ";
-     int i;
-     cin >> i;
-     pushBack(dl, i);
-    }
-    else if(option == 3){
-     cout << "One element poped from front\n";
-     popBack(dl);
-    }
-    else if(option == 4){
-     cout << "One element poped from back\n";
-     popBack(dl);
-    }
-    else if(option == 0){
-      cout << "The final list is:\n";
-      break;
-    }
-    else{
-        cout << "Invalid input\n";
-    }
-   }
+    list<int> dl;
 
-   for(auto i: dl){
+    cout << "-----------------\n";
+    cout << "1. Push Front\n2. Push Back\n3. Pop Front\n4. Pop back\n0. Exit\n";
+
+    for (int a = true; a != 0;)
+    {
+        cout << "Select an option\n";
+        int option;
+        cin >> option;
+        if (option == 1)
+        {
+            cout << "Enter the element: ";
+            int i;
+            cin >> i;
+            pushFront(dl, i);
+        }
+        else if (option == 2)
+        {
+            cout << "Enter the element: ";
+            int i;
+            cin >> i;
+            pushBack(dl, i);
+        }
+        else if (option == 3)
+        {
+            cout << "One element poped from front\n";
+            popBack(dl);
+        }
+        else if (option == 4)
+        {
+            cout << "One element poped from back\n";
+            popBack(dl);
+        }
+        else if (option == 0)
+        {
+            cout << "The final list is:\n";
+            break;
+        }
+        else
+        {
+            cout << "Invalid input\n";
+        }
+    }
+
+    for (auto i : dl)
+    {
         cout << i << " ";
-   }
+    }
 
-   return 0;
+    return 0;
 }
 
-void pushFront(list<int> &dl, int n){
+void pushFront(list<int> &dl, int n)
+{
 
-        dl.push_front(n);
-    
+    dl.push_front(n);
 }
 
+void pushBack(list<int> &dl, int n)
+{
 
-void pushBack(list<int> &dl, int n){
-
-        dl.push_back(n);
-    
+    dl.push_back(n);
 }
 
+void popFront(list<int> &dl)
+{
 
-void popFront(list<int> &dl){
-
-        dl.pop_front();
-    
+    dl.pop_front();
 }
 
+void popBack(list<int> &dl)
+{
 
-void popBack(list<int> &dl){
-    
-        dl.pop_back();
-    
+    dl.pop_back();
 }
