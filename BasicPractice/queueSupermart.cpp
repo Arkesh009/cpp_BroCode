820c8f3b3bb4db9766e2ebdc062aabc68a3ae574
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-16 23:54:48 +0530

    FCFS queue

---
+++ BasicPractice/queueSupermart.cpp
@@ -0,0 +1,28 @@
+// Write a program to simulate a queue in a supermarket where customers are served in a First-Come, First-Serve (FCFS) manner.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   queue<string> q;
+   cout << "Enter the number of people in the queue\n";
+   int n;
+   cin >> n;
+
+   cout << "Enter the names of the customers lined up in the queue\n";
+   do{
+        string s;
+        getline(cin,s);
+        q.emplace(s);
+   }while(n--);
+
+   cout << "The order of the FCFS: ";
+
+   while(!q.empty()){
+        cout << q.front()<< " ";
+        q.pop();
+   }
+
+   return 0;
+}
