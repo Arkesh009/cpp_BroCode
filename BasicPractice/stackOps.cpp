c3cc4da1aec6eaba4cafcfb1a291999fc6aae922
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-16 23:30:25 +0530

    stack ops

--- BasicPractice/stackOps.cpp
+++ BasicPractice/stackOps.cpp
@@ -1,37 +1,45 @@
 // Implement a stack using stack that supports the following operations: push, pop, peek, and check if the stack is empty.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   stack<int> s;
-   int n;
-   // basic operations
-   cout << "Enter the number of elements to push in stack: ";
-   cin >> n;
-   cout << "Enter the elements\n";
-   while(n--){
+    stack<int> s;
+    int n;
+    // basic operations
+    // Push
+    cout << "Enter the number of elements to push in stack: ";
+    cin >> n;
+    cout << "Enter the elements\n";
+    while (n--)
+    {
         int i;
         cin >> i;
         s.push(i);
-   }
+    }
 
-   cout << "Enter the number of elements you wanna pop from stack: ";
-   int a;
-   cin >> a;
-   while(a--){
+    // POP
+    cout << "Enter the number of elements you wanna pop from stack: ";
+    int a;
+    cin >> a;
+    while (a--)
+    {
         s.pop();
-   }
+    }
 
-   cout << "The element at the top of the stack = " << s.top() << "\n";
+    // Peek
+    cout << "The element at the top of the stack = " << s.top() << "\n";
 
-   // check if stack is empty
-   if(!s.empty()){
+    // check if stack is empty
+    if (!s.empty())
+    {
         cout << "The stack isn't empty\n";
-   }else{
+    }
+    else
+    {
         cout << "The stack is empty\n";
-   }
-   
-   return 0;
+    }
+
+    return 0;
 }
