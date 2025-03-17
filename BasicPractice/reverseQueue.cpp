098ce926bb12a56f14b3b4685acf43fcaa27496d
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-17 22:56:59 +0530

    reverse queue

--- BasicPractice/reverseQueue.cpp
+++ BasicPractice/reverseQueue.cpp
@@ -1,39 +1,49 @@
 // Implement a queue using queue to reverse the first k elements of a queue.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   queue<int> q;
-   vector<int> v;
+    queue<int> q;
+    vector<int> v;
 
-   cout << "Enter the size of queue\n";
-   int n;
-   cin >> n;
+    // input queue size
+    cout << "Enter the size of queue\n";
+    int n;
+    cin >> n;
 
-   cout << "Enter the elements of queue\n";
-   while(n--){
+    // input queue elements
+    cout << "Enter the elements of queue\n";
+    while (n--)
+    {
         int a;
         cin >> a;
         q.emplace(a);
-   }
+    }
 
-   cout << "Enter the number of terms you wanna reverse\n";
-   int x;
-   cin >> x;
-   int size = q.size()-x;
-   while(x--){
+    // input number of terms to be reversed
+    cout << "Enter the number of terms you wanna reverse\n";
+    int x;
+    cin >> x;
+
+    // push elements of queue upto x terms in a vector
+    while (x--)
+    {
         v.push_back(q.front());
         q.pop();
-   }
+    }
 
-   reverse(v.begin(), v.end());
+    // reverse the vector
+    reverse(v.begin(), v.end());
 
-   for(auto i: v){
+    // print the vector i.e reversed elements of queue
+    for (auto i : v)
+    {
         cout << i << " ";
-   }
+    }
 
+    // To complete the process we can push reversed terms back inside the queue
 
-   return 0;
+    return 0;
 }
