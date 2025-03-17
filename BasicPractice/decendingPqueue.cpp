1271a0998dda812ff206ed985b32a8a7c529482f
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-17 23:10:16 +0530

    pq decending

--- BasicPractice/decendingPqueue.cpp
+++ BasicPractice/decendingPqueue.cpp
@@ -1,27 +1,32 @@
 // Implement a priority queue that stores integers and print them in descending order using the priority_queue class.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   priority_queue<int> pq;
-   cout << "Enter the size of pq\n";
-   int n;
-   cin >> n;
+    priority_queue<int> pq;
+    // input pq size
+    cout << "Enter the size of pq\n";
+    int n;
+    cin >> n;
 
-   cout << "Enter the elements in pq\n";
-   while(n--){
+    // input pq elements
+    cout << "Enter the elements in pq\n";
+    while (n--)
+    {
         int a;
         cin >> a;
         pq.emplace(a);
-   }
+    }
 
-   cout << "The pq in decending order: ";
-   while(!pq.empty()){
+    // print pq in decending order
+    cout << "The pq in decending order: ";
+    while (!pq.empty())
+    {
         cout << pq.top() << " ";
         pq.pop();
-   }
+    }
 
-   return 0;
+    return 0;
 }
