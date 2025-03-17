43bbe647587b416b4638d64236778fe5c5fee600
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-17 23:27:31 +0530

    K largest PQ

--- BasicPractice/kLargestPqueue.cpp
+++ BasicPractice/kLargestPqueue.cpp
@@ -1,26 +1,41 @@
 // Solve the problem of finding the k largest elements in an array using a priority_queue.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    vector<int> v = {2,5,7,6,3,4,1,8};
+    // dynamic array & pq declaration
+    vector<int> v = {2, 5, 7, 6, 3, 4, 1, 8};
     priority_queue<int> pq;
 
-    for(auto i: v){
+    // pushing array inside pq
+    // pq is automatically sorted structure
+    // puts larger value above the smaller
+    for (auto i : v)
+    {
         pq.emplace(i);
     }
 
+    // input the demand
     cout << "Enter the number of largest element to print\n";
-    int n;
-    cin >> n;
+    int k;
+    cin >> k;
 
-    cout << "The " << n << " largest elements of the array: ";
-    while(n--){
+    // check if k is valid
+    if (k > pq.size() || k <= 0)
+    {
+        cout << "Invalid input";
+        return -1;
+    }
+
+    // printing the k largest elements
+    cout << "The " << k << " largest elements of the array: ";
+    while (k--)
+    {
         cout << pq.top() << " ";
         pq.pop();
     }
-    
-   return 0;
+
+    return 0;
 }
