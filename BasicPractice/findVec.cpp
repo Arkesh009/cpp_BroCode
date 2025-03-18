f07e4f84860ee4163bd9f2ccf8f62267a8f0b26d
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-18 22:06:43 +0530

    find n

--- BasicPractice/findVec.cpp
+++ BasicPractice/findVec.cpp
@@ -1,19 +1,22 @@
 // Use find() to search for a specific element in a vector and print its index.
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   vector<int> v = {1,2,3,4,5,6,7,8};
+    // vec declaration
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
 
-   cout << "Enter the element bw 1 to 8 to find its index\n";
+    cout << "Enter the element bw 1 to 8 to find its index\n";
 
-   int n;
-   cin >> n;
+    // input the target
+    int n;
+    cin >> n;
 
-   auto a = find(v.begin(), v.end(), n);
-   cout << "The index = " << *a;
+    // save addres of index of target usin find()
+    auto a = find(v.begin(), v.end(), n);
+    cout << "The index = " << *a;
 
-   return 0;
+    return 0;
 }
