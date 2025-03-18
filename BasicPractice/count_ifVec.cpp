67ae4359970cf2ea228b43e375730e95e4c66128
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-18 22:22:39 +0530

    count if

--- BasicPractice/count_ifVec.cpp
+++ BasicPractice/count_ifVec.cpp
@@ -1,19 +1,21 @@
 // Implement a program that counts the number of elements greater than a given number in a vector using count_if().
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   vector<int> v = {1,2,3,4,5,6,7,8};
-   int target;
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+    int target;
 
-   cout << "Enter the target\n";
-   cin >> target;
+    cout << "Enter the target\n";
+    cin >> target;
 
-   auto it = count_if(v.begin(), v.end(), [target](int x){return x>target;});
-   
-   cout << it;
-   
-   return 0;
+    // Use count_if with a lambda function to count elements greater than the target
+    auto it = count_if(v.begin(), v.end(), [target](int x)
+                       { return x > target; });
+
+    cout << it;
+
+    return 0;
 }
