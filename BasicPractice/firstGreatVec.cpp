0422fec68d5ee07c6ccf05c1282e1ba80a6be2de
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-20 00:24:27 +0530

    next greater

--- BasicPractice/firstGreatVec.cpp
+++ BasicPractice/firstGreatVec.cpp
@@ -1,18 +1,28 @@
 // Find the First Element Greater Than a Target
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   vector<int> v = {1,2,3,4,5,6,7,8};
-    int target = 3;
-   for(auto i: v){
-        if(i>target){
+    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    cout << "Enter the target\n";
+    int target;
+    cin >> target;
+
+    cout << "The num just greater than the target: ";
+    auto m = max(v.begin(), v.end());
+
+    for (auto i : v)
+    {
+        if (i > target)
+        {
             cout << i;
-            break;
-            
+            return 0;
         }
-   }
-   return 0;
+    }
+
+
+    return 0;
 }
