620884971c2eb96ce851cba9fb0d77cc825e4558
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-19 23:57:52 +0530

    find even

--- BasicPractice/countEvenVec.cpp
+++ BasicPractice/countEvenVec.cpp
@@ -1,30 +1,33 @@
 // Count Even Numbers in a Vector
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   vector<int> v;
-   cout << "Enter the size of array\n";
-   int n;
-   cin >> n;
+    vector<int> v;
+    cout << "Enter the size of array\n";
+    int n;
+    cin >> n;
 
-   cout << "Enter the elements in the vec\n";
-   while(n--){
+    cout << "Enter the elements in the vec\n";
+    while (n--)
+    {
         int a;
         cin >> a;
         v.push_back(a);
-   }
+    }
 
-   int count = 0;
-   for(auto i: v){
-        if(i%2 == 0){
+    int count = 0;
+    for (auto i : v)
+    {
+        if (i % 2 == 0)
+        {
             count++;
         }
-   }
+    }
 
-   cout << "The number of even terms in the array: " << count;
-   
-   return 0;
+    cout << "The number of even terms in the array: " << count;
+
+    return 0;
 }
