d6ea89605609717b5756d18024489b92bcf8ef6d
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-21 23:10:24 +0530

    sstream

--- BasicPractice/breakString.cpp
+++ BasicPractice/breakString.cpp
@@ -1,18 +1,19 @@
 // Breakdown sentance into words using sstream
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     string s, word;
     cout << "Enter the sentence\n";
-    getline(cin,s);
-   stringstream ss(s);
+    getline(cin, s);
+    stringstream ss(s);
 
-   while(ss >> word){
+    while (ss >> word)
+    {
         cout << word << "\n"; // print each word in nextline
-   }
-   
-   return 0;
+    }
+
+    return 0;
 }
