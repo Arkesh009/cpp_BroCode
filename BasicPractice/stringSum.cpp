e642b36d0b3d139e60169c0c9d03a079b3ebd74a
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-22 23:59:51 +0530

    n

--- BasicPractice/stringSum.cpp
+++ BasicPractice/stringSum.cpp
@@ -1,23 +1,24 @@
 // Convert String to Integers and Find Sum
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   string s;
-   cout << "Enter the space seperated string of numbers\n";
-   getline(cin, s);
+    string s;
+    cout << "Enter the space seperated string of numbers\n";
+    getline(cin, s);
 
-   int token;
+    int token;
     stringstream ss(s);
     int sum = 0;
 
-    while(ss >> token){
+    while (ss >> token)
+    {
         sum += token;
     }
 
     cout << "The sum of all integers in the string: ";
-   
-   return 0;
+
+    return 0;
 }
