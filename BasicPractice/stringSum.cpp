651baf5f9febdb1158c04bf0fcb6eec5c4216816
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-23 00:23:02 +0530

    string sum

--- BasicPractice/stringSum.cpp
+++ BasicPractice/stringSum.cpp
@@ -9,16 +9,19 @@ int main()
     cout << "Enter the space seperated string of numbers\n";
     getline(cin, s);
 
+    // token to read integer values from stream
     int token;
     stringstream ss(s);
     int sum = 0;
 
+    // inserting int value inside the token
     while (ss >> token)
     {
         sum += token;
     }
 
-    cout << "The sum of all integers in the string: ";
+    // output
+    cout << "The sum of all integers in the string: " << sum;
 
     return 0;
 }
