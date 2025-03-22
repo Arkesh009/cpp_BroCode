27f0ac4a4894dfe580747e5b02888bb2859d7b64
Arkesh009 <arkeshbhargava@gmail.com>
2025-03-22 23:52:03 +0530

    dynamic str

--- BasicPractice/dynamicString.cpp
+++ BasicPractice/dynamicString.cpp
@@ -1,20 +1,21 @@
 // Building a String Dynamically
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-   stringstream ss;
-
-   int age = 25, dob = 1999;
+    stringstream ss;
+    // data
+    int age = 25, dob = 1999;
     double cgpa = 70.4;
     string name = "Arkesh";
-   
-   ss << "My name is " << name << " I am " << dob << " born " << "so my age is " << age << " and my CGPA is " << cgpa;
+    // inserting more info inside the stream of string
+    ss << "My name is " << name << " I am " << dob << " born " << "so my age is " << age << " and my CGPA is " << cgpa;
+    // converting stream into a string
+    string result = ss.str();
+    // output string
+    cout << result;
 
-   string result = ss.str();
-   cout << result;
-
-   return 0;
+    return 0;
 }
