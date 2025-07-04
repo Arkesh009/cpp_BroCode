86d674909ac5921c7644d8886905db43e835b420
Arkesh009 <arkeshbhargava@gmail.com>
2025-07-04 09:00:00 +0530

    Temprature convertor

--- Rivision/03.cpp
+++ Rivision/03.cpp
@@ -2,7 +2,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     double temp;
     char unit;
     cout << "*******Temprature Conversion Scale*******\n";
@@ -11,19 +12,22 @@ int main(){
     cout << "Choose the unit to convert the value into\n";
     cin >> unit;
 
-    if(unit == 'F' || unit == 'f'){
+    if (unit == 'F' || unit == 'f')
+    {
         cout << "Enter the temp in celcius\n";
         cin >> temp;
-        temp = (1.8*temp) + 32;
+        temp = (1.8 * temp) + 32;
         cout << "Temprature in Farhenheit = " << temp << "\n";
     }
-    else if(unit = 'C' || unit == 'c'){
+    else if (unit = 'C' || unit == 'c')
+    {
         cout << "Enter the temp in Farhenheit\n";
         cin >> temp;
         temp = (temp - 32) / 1.8;
         cout << "Temprature in Celcius = " << temp << "\n";
     }
-    else{
+    else
+    {
         cout << "Invalid input\n";
     }
 
