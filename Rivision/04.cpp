e8685928b094c0accd8f23f020540761ef46d125
Arkesh009 <arkeshbhargava@gmail.com>
2025-07-05 09:00:00 +0530

    Rand cal

---
+++ Rivision/04.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num;
+    int guess;
+    int tries = 0;
+
+    srand(time(NULL));
+    num = (rand() % 100) + 1;
+
+    cout << "****** Number Guessing Game*******\n";
+    cout << "Guess the number between 1-100\n";
+   
+
+    do{
+        cin >> guess;
+        
+        if(guess > num){
+            cout << "Too High\n";
+        }else if(guess < num){
+            cout << "Too low\n";
+        }else{
+            cout << "CORRECT! " << "\nTries = " << tries;
+        }
+        tries++;
+    }while(guess != num);
+
+    return 0;
+}
